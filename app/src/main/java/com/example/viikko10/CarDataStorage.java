1b1a08ba09ed1a22965920610a5ce3754b73707e
Norpatin <nooravep@gmail.com>
2025-03-20 12:08:23 +0200

    Versio 1

---
+++ app/src/main/java/com/example/viikko10/CarDataStorage.java
@@ -0,0 +1,47 @@
+package com.example.viikko10;
+
+import java.util.ArrayList;
+
+public class CarDataStorage {
+
+    String city;
+    int year;
+    private ArrayList<CarData> carDatas = new ArrayList<>();
+    private static CarDataStorage carDataStorage = null;
+
+    private CarDataStorage(){
+    }
+
+    static public CarDataStorage getInstance() {
+        if(carDataStorage == null) {
+            carDataStorage = new CarDataStorage();
+        }
+        return carDataStorage;
+    }
+
+    public ArrayList<CarData> getCarData() {
+        return carDatas;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void clearData() {/*Code here*/}
+
+    public String getCity() {
+        return city;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void addCarData(CarData carData) {
+        carDatas.add(carData);
+    }
+}
---
+++ app/src/main/java/com/example/viikko10/SearchActivity.java
@@ -0,0 +1,38 @@
+package com.example.viikko10;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private EditText CityName;
+    private EditText Year;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_search);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        CityName = findViewById(R.id.CityNameEdit);
+        Year = findViewById(R.id.YearEdit);
+    }
+
+    public void SwitchToListInfoActivity(View view){
+        Intent intent = new Intent(this, ListInfoActivity.class);
+        startActivity(intent);
+    }
+
+}
